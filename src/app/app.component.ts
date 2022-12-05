c7e94ab971499b8420953a0d63fc82b55310fe3b
Rashmi <rashmijayasekara00@gmail.com>
2022-12-05 13:17:28 +0530

    Complete the UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'upload-front-end';
+  imageUrl: string='';
+
+
+  updatePic(eventData: Event) :void{
+   const file= (eventData.target! as HTMLInputElement).files!.item(0);  // safe navigation operator
+    // Blob - URL convertion
+    if(file) {
+     this.imageUrl= `url(${URL.createObjectURL(file)})`; // we have to give that this file won't be null
+    }else {
+      this.imageUrl="";
+    }
+  }
+
+  removePic(file: HTMLInputElement) {
+    file.value="";
+    this.imageUrl='';
+
+  }
 }
