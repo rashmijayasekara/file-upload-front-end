a1f47525dbb467a507d7a910053a6bbf55288baf
Rashmi <rashmijayasekara00@gmail.com>
2022-12-05 13:37:36 +0530

    Complet the Functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,22 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  private readonly apiUrl:string='http://localhost:8080/upload-app/api/uploads'
   imageUrl: string='';
+  fullName:string='';
+  username:string='';
+  password:string='';
 
+
+  constructor(private http:HttpClient) {
+  }
 
   updatePic(eventData: Event) :void{
    const file= (eventData.target! as HTMLInputElement).files!.item(0);  // safe navigation operator
     // Blob - URL convertion
     if(file) {
-     this.imageUrl= `url(${URL.createObjectURL(file)})`; // we have to give that this file won't be null
+     this.imageUrl= `url(${URL.createObjectURL(file)})`;
     }else {
       this.imageUrl="";
     }
   }
 
-  removePic(file: HTMLInputElement) {
+  removePic(file: HTMLInputElement) :void{
     file.value="";
     this.imageUrl='';
 
+  }
+
+  signUp(file: File| null) :void{
+    const formData=new FormData();
+    formData.append('fullName',this.fullName);
+    formData.append('username',this.username);
+    formData.append('password',this.password);
+    if (file)formData.append('profilePic',file);
+    this.http.post(this.apiUrl, formData).subscribe();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
